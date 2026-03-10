3b0607e7c6b783c57729a091c808cf9e1464728b
Alluri Aditya <your_email@gmail.com>
2026-03-10 22:16:47 +0530

    Added square root of x solution

---
+++ BinarySearch/ClassicBinarysearch/069_sqrtofX.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+        int ans =0;
+        while(start<=end)  {
+            int mid = start + (end-start)/2;
+            long sqr = (long) mid*mid;
+            if(sqr == x) {
+                return mid;
+            } else if(sqr<x) {
+                ans = mid;
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+}
